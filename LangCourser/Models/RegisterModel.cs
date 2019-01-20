c4edd1c99122952b6e3779ab92d08652786fe813
Daniel <daniel.sikora96@gmail.com>
2019-01-21 00:45:53 +0100

    Cleanup and corrected RegisterModel

--- LangCourser/Models/RegisterModel.cs
+++ LangCourser/Models/RegisterModel.cs
@@ -1,31 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Web;
-using System.Web.ModelBinding;
-using ISBD_project.Resources;
 
 namespace ISBD_project.Models
 {
     public class RegisterModel
     {
+        [Required]
+        [StringLength(64, MinimumLength = 6)]
         public string LoginA { get; set; }
 
-        [StringLength(6)]
+        [StringLength(64, MinimumLength = 6)]
+        [Required]
         public string PasswordA { get; set; }
 
         [Compare("PasswordA")]
+        [Required]
         public string RepeatedPasswordA { get; set; }
 
         public string NameU { get; set; }
         
         public string SurnameU { get; set; }
         
+        [Required]
         public string EmailU { get; set; }
 
         public DateTime BirthDate { get; set; }
 
+        [Range(0,1)]
         public int GenderU { get; set; }
 
         public int? IdUa { get; set; } = 3;
